4f85474792bfca4d13956ecf481b82a34b0ddbb9
Vvivelle <dorota.bialowas@gmail.com>
2024-12-22 18:03:57 +0100

    poprawione

---
+++ app.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react'
+
+import './Kalkulator.css'
+funcion App() {
+
+    const [długość, setDługość] = useState(0)
+    const [szerokość, setSzerokość] = useState(0)
+    const [wysokość, setWysokość] = useState(0)
+    const [ilość_okien , setIlość_okien] = useState(0)
+    const [ilpość_drzwi, setIlość_drzwi] = useState(0)
+    const [rodzaj_farby, setRodzaj_farby = useState(0)
+    const [ilość_warstw, setIlość_warstw] = useState(0)
+        const [oblicz, setOblicz] = useState(**)
+            const [Potrzebujesz, setPotrzebujesz] = useState(**)
+
+            let imgSrc =**
+
+
+    return (
+        <div className={"app">
+        <div className={"container"}>
+            <h2 className="center">KALKULATOR fARB</h2>
+        <form>
+            <div>
+            <label>długość (m2)</label>
+            <input>value={długość} />
+        </div>
+
+            );
+        }
+
+        export default App;
